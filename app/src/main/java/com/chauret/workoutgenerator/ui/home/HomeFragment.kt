c99fbbf53fde4486fdfacb7cd5b3568a9b46da8f
Ren Chauret <renaudchauret@gmail.com>
2022-10-26 09:31:26 -0400

    add chips

--- app/src/main/java/com/chauret/workoutgenerator/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chauret/workoutgenerator/ui/home/HomeFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.chauret.workoutgenerator.R
 import com.chauret.workoutgenerator.databinding.FragmentHomeBinding
+import com.chauret.workoutgenerator.model.movement.WorkoutType
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.google.android.material.slider.RangeSlider
+import java.io.ObjectInputStream
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +22,10 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val WORKOUT_TYPES_FILENAME = "workout_generator_workout_types"
+
+    private var workoutTypes: Set<WorkoutType> = setOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +38,55 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.generateTitle
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+        workoutTypes = loadWorkoutTypes()
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val selectWorkoutTypesChipGroup: ChipGroup = view.findViewById(R.id.selectWorkoutTypesChipGroup)
+        workoutTypes.forEach {
+            val workoutTypeChip = Chip(this.context)
+            workoutTypeChip.text = it.name
+            workoutTypeChip.isCheckable = true
+            workoutTypeChip.checkedIcon = null
+            workoutTypeChip.chipBackgroundColor
+            selectWorkoutTypesChipGroup.addView(workoutTypeChip)
+        }
+
+        val selectExercisesRangeSlider: RangeSlider = view.findViewById(R.id.selectExercisesRangeSlider)
+        selectExercisesRangeSlider.values = listOf(3f, 5f)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun loadWorkoutTypes(): Set<WorkoutType> {
+        try {
+            val fis = requireActivity().openFileInput(WORKOUT_TYPES_FILENAME)
+            val ois = ObjectInputStream(fis)
+            val workoutTypes: Set<WorkoutType> = ois.readObject() as Set<WorkoutType>
+            fis.close()
+            ois.close()
+            return workoutTypes
+        } catch (e: Exception) {
+            println("workout_generator_workout_types file not found, creating new")
+        }
+        return setOf(
+            WorkoutType(UUID.randomUUID(), "Chest"),
+            WorkoutType(UUID.randomUUID(), "Shoulders"),
+            WorkoutType(UUID.randomUUID(), "Back"),
+            WorkoutType(UUID.randomUUID(), "Legs"),
+            WorkoutType(UUID.randomUUID(), "Arms")
+        )
     }
 }
