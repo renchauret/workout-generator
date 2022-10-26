285aacd1eb703937d5fc54c88427ff2e6a05404c
Ren Chauret <renaudchauret@gmail.com>
2022-10-26 09:44:27 -0400

    more renaming

--- app/src/main/java/com/chauret/workoutgenerator/MainActivity.kt
+++ app/src/main/java/com/chauret/workoutgenerator/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_generate, R.id.navigation_history, R.id.navigation_movements
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
